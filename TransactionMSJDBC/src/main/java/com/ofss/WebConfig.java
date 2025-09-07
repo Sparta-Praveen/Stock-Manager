fec52bd6d24832d154767b1360b99c93cdc7a1e8
Sparta-Praveen <150798283+Sparta-Praveen@users.noreply.github.com>
2025-09-07 19:42:21 +0530

    Add files via upload

---
+++ TransactionMSJDBC/src/main/java/com/ofss/WebConfig.java
@@ -0,0 +1,47 @@
+//package com.ofss;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//class WebConfig {
+//	
+//	@Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//		System.out.println("WebConfig - corsConfigurer method called - CORS enabled");
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")  // Allow CORS on all endpoints
+//                        .allowedOrigins("http://10.120.159.41:8086") // Your frontend origin
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH") // Allowed HTTP methods
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//
+//}
+
+package com.ofss;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // adjust the path as needed
+                        .allowedOrigins("http://localhost:8000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

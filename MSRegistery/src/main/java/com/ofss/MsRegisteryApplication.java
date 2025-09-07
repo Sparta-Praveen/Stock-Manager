ee99767243b1be9b36c774db18d6487e3dd9a280
Sparta-Praveen <150798283+Sparta-Praveen@users.noreply.github.com>
2025-09-07 19:44:10 +0530

    Add files via upload

---
+++ MSRegistery/src/main/java/com/ofss/MsRegisteryApplication.java
@@ -0,0 +1,15 @@
+package com.ofss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRegisteryApplication.class, args);
+	}
+
+}
